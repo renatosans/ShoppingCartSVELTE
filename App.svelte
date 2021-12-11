d0efc4ac20c0527123e0504b988ca301467e9c3f
renato_sans <renato@datacopy.com.br>
2021-12-11 11:42:47 -0300

    Commit inicial do projeto

---
+++ App.svelte
@@ -0,0 +1,63 @@
+
+<script>
+    import { onMount } from 'svelte';
+    import { slide } from 'svelte/transition';
+    import CartItem from './componentes/CartItem.svelte';
+    import NumberSpinner from './componentes/NumberSpinner.svelte';
+    import ProductCatalog from './componentes/ProductCatalog.svelte';
+
+
+    export let name;
+    let dadosProdutos = [];
+    let shoppingCart = { cliente:"Cliente 1", total: 0, produtosAdicionados: [] };
+
+
+	onMount(async () => {
+        // axios.get('mock_data/listaProdutos.json').then( response => { renderizar(response.data); });
+		const res = await fetch('mock_data/listaProdutos.json');
+		dadosProdutos = await res.json();
+	});
+
+    let showItems = false;
+</script>
+
+<style>
+    .cart{
+        position: fixed;
+        top: 0;
+        right: 0;
+    }
+
+    input[type="checkbox"] {
+        width: 30px;
+        height: 30px;
+        appearance: none;
+        background-color: white;
+        background-image: url('./icons/cart.svg');
+    }
+
+    input[type="checkbox"]:checked {
+        background-color: green;
+    }
+</style>
+
+<main>
+    <div class="container">
+        <div id="grid" class="grid">
+            {#each dadosProdutos as produto}
+                <ProductCatalog produto={produto} shoppingCart={shoppingCart}></ProductCatalog>
+            {/each}
+        </div>
+    </div>
+</main>
+
+<div class="cart">
+    <input type="checkbox" bind:checked={showItems}>
+    {#if showItems}
+        {#each shoppingCart.produtosAdicionados as produto}
+            <div transition:slide>
+                <CartItem produto={produto} quantidade=1></CartItem>
+            </div>
+        {/each}
+    {/if}
+</div>
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+
+var path = require('path');
+
+
+module.exports = {
+  mode: 'none',
+  entry: './main.js',
+  output: { path: __dirname, filename: 'bundle.js' },
+  resolve: {
+    // see below for an explanation
+    alias: {
+      svelte: path.resolve('node_modules', 'svelte')
+    },
+    extensions: ['.mjs', '.js', '.svelte'],
+    mainFields: ['svelte', 'browser', 'module', 'main']
+  },  
+  module: {
+    rules: [
+      {
+        test: /\.(html|svelte)$/,
+        use: 'svelte-loader'
+      },
+      {
+        // required to prevent errors from Svelte on Webpack 5+, omit on Webpack 4
+        test: /node_modules\/svelte\/.*\.mjs$/,
+        resolve: {
+          fullySpecified: false
+        }
+      }
+    ]
+  }
+};
